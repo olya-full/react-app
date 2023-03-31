83ef9cba807ef282a0a07f1ca389c9eddbba5f16
Olya Y <f_akora@mail.ru>
2023-03-31 23:16:24 +0300

    refactor: change Header and Nav components to functions

--- react-ts-app/src/components/Header/Header.tsx
+++ react-ts-app/src/components/Header/Header.tsx
@@ -1,31 +1,26 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
-export class Header extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div className="header">
-        <Nav />
-      </div>
-    );
-  }
+export const Header = () => {
+  return (
+    <div className="header">
+      <Nav />
+    </div>
+  );
 }
 
-class Nav extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <nav className="header-nav">
-        <NavLink className="header-nav-item" to="/">
-          Home
-        </NavLink>
-        <NavLink className="header-nav-item" to="/form">
-          Form
-        </NavLink>
-        <NavLink className="header-nav-item" to="/about">
-          About Us
-        </NavLink>
-      </nav>
-    );
-  }
+const Nav = () => {
+  return (
+    <nav className="header-nav">
+      <NavLink className="header-nav-item" to="/">
+        Home
+      </NavLink>
+      <NavLink className="header-nav-item" to="/form">
+        Form
+      </NavLink>
+      <NavLink className="header-nav-item" to="/about">
+        About Us
+      </NavLink>
+    </nav>
+  );
 }
