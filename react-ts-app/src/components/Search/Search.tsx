e0b492fa47ade7c0ac4cc569b9cd23a1ee74dc7e
Olya Y <f_akora@mail.ru>
2023-04-06 20:42:12 +0300

    feat: lay groundwork for api requests and rendering search results

--- react-ts-app/src/components/Search/Search.tsx
+++ react-ts-app/src/components/Search/Search.tsx
@@ -1,8 +1,14 @@
 import React, { ChangeEvent } from "react";
 import "./Search.css";
 import Button from "../Utils/Button/Button";
+import { ISearchResult } from "../../pages/HomePage";
 
-export const SearchBar = () => {
+export interface ISearchElemProps {
+  renderResults: (result: ISearchResult[]) => void;
+}
+
+export const SearchElem = (props: ISearchElemProps) => {
+  const apiEndpoint = "https://www.flickr.com/services/rest/";
   const [inputValue, setInputValue] = React.useState(localStorage.getItem("bestSearchValue") || "");
   const inputRef = React.useRef(inputValue);
 
@@ -17,6 +23,24 @@ export const SearchBar = () => {
     []
   );
 
+  const sendSearchReq = async (text: string) => {};
+
+  const handleSubmit = (text: string) => {
+    const newSearch = [
+      {
+        id: "123456",
+        title: "SomeTitle",
+        imageUrl:
+          "https://sun9-8.userapi.com/impg/3flHO-BUQcdXG1hclYqaO6qQyQ3WjOPUEWJepw/0yf_I_4ODCE.jpg?size=130x87&quality=96&sign=d1f08d5b30fba524b3c6922e38b3e64a&c_uniq_tag=LYlKW0-Hk8jJNvyZh7q7CxjX-xTzaJToZa8LT3yhzjM&type=album",
+        date: new Date(1395660658),
+        author: "Some Author",
+        location: "Berlin",
+      },
+    ];
+
+    props.renderResults(newSearch);
+  };
+
   const updateInputValue = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
@@ -26,7 +50,7 @@ export const SearchBar = () => {
       <input
         type="search"
         className="home-search"
-        placeholder="🔎︎"
+        placeholder="🔎︎ Search by picture title"
         value={inputValue}
         onChange={(event) => updateInputValue(event)}
       />
--- react-ts-app/src/pages/HomePage.tsx
+++ react-ts-app/src/pages/HomePage.tsx
@@ -1,10 +1,28 @@
+import React from "react";
 import { Cards } from "../components/Cards/Cards";
-import { SearchBar } from "../components/Search/Search";
+import { SearchElem } from "../components/Search/Search";
 
-export const Home = () => (
-  <>
-    <h1>Picture Search</h1>
-    <SearchBar />
-    <Cards />
-  </>
-);
+export interface ISearchResult {
+  id: string;
+  title: string;
+  imageUrl: string;
+  date: Date;
+  author: string;
+  location: string;
+}
+
+export const Home = () => {
+  const [searchResults, setSearchResults] = React.useState<ISearchResult[]>([]);
+
+  const renderSeachResults = (newResult: ISearchResult[]) => {
+    setSearchResults([...newResult]);
+  };
+
+  return (
+    <>
+      <h1>Picture Search</h1>
+      <SearchElem renderResults={renderSeachResults} />
+      <Cards />
+    </>
+  );
+};
