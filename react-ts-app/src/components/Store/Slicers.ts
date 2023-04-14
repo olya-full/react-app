a5b7bbe6091d2e1f33039b30a1da266bdfc494f1
Olya Y <f_akora@mail.ru>
2023-04-14 18:27:42 +0300

    feat: add reducer in valid format

---
+++ react-ts-app/src/components/Store/Slicers.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const createdCardsSlice = createSlice({
+  name: "createdCards",
+  initialState: {
+    value: []
+  },
+  reducers: {
+
+  }
+})
+
--- react-ts-app/src/components/Store/Store.ts
+++ react-ts-app/src/components/Store/Store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createdCardsSlice } from "./Slicers";
 
 export default configureStore({
   reducer: {
-    
+    createdCards: createdCardsSlice.reducer,
   }
 })
