d81d07b8943c1e3b576ab039bc7f537dc51979a5
Olya Y <f_akora@mail.ru>
2023-04-22 00:04:34 +0300

    feat: start setting up server

---
+++ react-ts-app/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  
+})
--- react-ts-app/src/entry-client.tsx
+++ react-ts-app/src/entry-client.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { WrappedApp } from "./components/App/App";
 import "./main.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.hydrateRoot(document.getElementById("root") as HTMLElement,
   <React.StrictMode>
     <WrappedApp />
   </React.StrictMode>
